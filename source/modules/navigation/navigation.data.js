1178fb7d9961f5c6b49bc54e6a47dbc8bfc4e3fb
Marta Pieczynska <marta.pieczynska@unic.com>
2016-04-01 15:09:54 +0200

    FHNW-2 Make the dropdown smarter with hbs, move content to the data file

--- source/modules/navigation/navigation.data.js
+++ source/modules/navigation/navigation.data.js
@@ -11,7 +11,91 @@ var data = _.merge(defaultData, {
 			description: '',
 			code: dataHelper.getTemplateCode('navigation.hbs'),
 			documentation: dataHelper.getDocumentation('navigation.md')
-		}
+		},
+		navItems: [
+			{
+				title: 'Studies',
+				link: '#',
+				subItems: [
+					{
+						title: 'Some section',
+						link: '#'
+					},
+					{
+						title: 'Another one',
+						link: '#'
+					}
+				]
+			},
+			{
+				title: 'Research',
+				link: '#',
+				subItems: [
+					{
+						title: 'Pokemon',
+						link: '#',
+						subItems: [
+							{
+								title: 'Pichu',
+								link: '#'
+							},
+							{
+								title: 'Pikachu',
+								link: '#'
+							},
+							{
+								title: 'Raichu',
+								link: '#'
+							}
+						]
+					}
+				]
+			},
+			{
+				title: 'Media',
+				link: '#',
+				subItems: [
+					{
+						title: 'Section again',
+						link: '#'
+					},
+					{
+						title: 'Some other stuff',
+						link: '#'
+					},
+					{
+						title: 'Another one',
+						link: '#'
+					}
+				]
+			},
+			{
+				title: 'Contact',
+				link: '#'
+			},
+			{
+				title: 'Other',
+				link: '#',
+				subItems: [
+					{
+						title: 'Blue/Red',
+						link: '#'
+					},
+					{
+						title: 'Yellow',
+						link: '#'
+					},
+					{
+						title: 'Gold/Silver',
+						link: '#'
+					},
+					{
+						title: 'Sapphire/Ruby',
+						link: '#'
+					}
+				]
+			}
+		]
 	});
 
 module.exports = data;
