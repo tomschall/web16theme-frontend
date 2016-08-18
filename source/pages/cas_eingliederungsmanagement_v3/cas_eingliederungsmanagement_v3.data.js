c1e2760aeecc6948eb030258fb28688b2633dcba
Marcel Bührig <marcel.buehrig@zeix.com>
2016-08-18 11:34:56 +0200

    Hide from Listing for Product Pages

--- source/pages/cas_eingliederungsmanagement_v3/cas_eingliederungsmanagement_v3.data.js
+++ source/pages/cas_eingliederungsmanagement_v3/cas_eingliederungsmanagement_v3.data.js
@@ -6,7 +6,8 @@ var _ = require('lodash'),
 
 var data = _.merge(defaultData, {
 		meta: {
-			title: 'CAS Eingliederungsmanagement v3'
+			title: 'CAS Eingliederungsmanagement v3',
+			hideFromListing: true
 		},
 		title: 'CAS Eingliederungsmanagement',
 		text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
