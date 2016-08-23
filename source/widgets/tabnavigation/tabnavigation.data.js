c06b4e2100104565e928b00821537098710914aa
Marcel Bührig <marcel.buehrig@zeix.com>
2016-08-23 18:46:54 +0200

    Possible fix for error js linting

--- source/widgets/tabnavigation/tabnavigation.data.js
+++ source/widgets/tabnavigation/tabnavigation.data.js
@@ -6,15 +6,15 @@ var _ = require('lodash'),
 	defaultData = requireNew('../../data/default.data.js');
 
 var data = _.merge(defaultData, {
-		meta: {
-			title: 'Tabulatoren | WI_011',
-			description: '',
-			code: dataHelper.getTemplateCode('tabnavigation.hbs'),
-			documentation: dataHelper.getDocumentation('tabnavigation.md'),
-			mocks: {
-				documentation: null
+			meta: {
+				title: 'Tabulatoren | WI_011',
+				description: '',
+				code: dataHelper.getTemplateCode('tabnavigation.hbs'),
+				documentation: dataHelper.getDocumentation('tabnavigation.md'),
+				mocks: {
+					documentation: null
+				}
 			}
-		}
-	});
+		});
 
 module.exports = data;
