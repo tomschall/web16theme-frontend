55a393253013cabc74e0dbdd6eadcc4cd7648e4a
Marcel Bührig <marcel.buehrig@zeix.com>
2016-08-23 09:31:08 +0200

    Fixed sidebar_application.data.js linting error

--- source/widgets/sidebar_application/sidebar_application.data.js
+++ source/widgets/sidebar_application/sidebar_application.data.js
@@ -6,25 +6,26 @@ var _ = require('lodash'),
 		defaultData = requireNew('../../data/default.data.js');
 
 var templateData = {
-	text: 'Das ist der Text für die Anmeldung',
-	note: 'Anmeldehinweis - eher klein',
-	button: {
-		text: 'Durchführung wählen',
-		url: '#gotoapplication'
-	}
-}, data = _.assign(defaultData, {
-	meta: {
-		title: 'Sidebar Application',
-		description: '',
-		code: dataHelper.getTemplateCode('sidebar_application.hbs'),
-		documentation: dataHelper.getDocumentation('sidebar_application.md'),
-		mocks: [
-			{
-				description: null,
-				data: dataHelper.getFormattedJSON(templateData)
+			text: 'Das ist der Text für die Anmeldung',
+			note: 'Anmeldehinweis - eher klein',
+			button: {
+				text: 'Durchführung wählen',
+				url: '#gotoapplication'
 			}
-		]
-	}
-}, templateData);
+		},
+		data = _.assign(defaultData, {
+			meta: {
+				title: 'Sidebar Application',
+				description: '',
+				code: dataHelper.getTemplateCode('sidebar_application.hbs'),
+				documentation: dataHelper.getDocumentation('sidebar_application.md'),
+				mocks: [
+					{
+						description: null,
+						data: dataHelper.getFormattedJSON(templateData)
+					}
+				]
+			}
+		}, templateData);
 
 module.exports = data;
