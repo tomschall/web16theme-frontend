61d11e5aa223e5f5f79c0744a6a1b27c04cbee77
Marcel Bührig <marcel.buehrig@zeix.com>
2016-08-23 18:04:32 +0200

    Fixed tabnavigation.data.js

--- source/widgets/tabnavigation/tabnavigation.data.js
+++ source/widgets/tabnavigation/tabnavigation.data.js
@@ -5,28 +5,16 @@ var _ = require('lodash'),
 	dataHelper = require('../../../helpers/data.js'),
 	defaultData = requireNew('../../data/default.data.js');
 
-var templateData = {
-	entries: [
-		{
-			type: 'contact',
-			title: 'Leitung',
-			name: 'contact',
-			templateData: _.assign(requireNew('../../widgets/contact/contact.data.js'), {})
-		}
-	]
-},
-
-data = _.merge(defaultData, {
+var data = _.merge(defaultData, {
 		meta: {
 			title: 'Tabulatoren | WI_011',
 			description: '',
 			code: dataHelper.getTemplateCode('tabnavigation.hbs'),
 			documentation: dataHelper.getDocumentation('tabnavigation.md'),
 			mocks: {
-				documentation: null,
-				data: dataHelper.getFormattedJSON(templateData)
+				documentation: null
 			}
 		}
-	}, templateData);
+	});
 
 module.exports = data;
