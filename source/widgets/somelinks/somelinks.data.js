acf9914b58d939aca4adc2d66f900bba37d32ea0
Marcel Bührig <marcel.buehrig@zeix.com>
2016-09-22 12:02:41 +0200

    Print.css added and FHNW-68 fixed

--- source/widgets/somelinks/somelinks.data.js
+++ source/widgets/somelinks/somelinks.data.js
@@ -5,13 +5,24 @@ var _ = require('lodash'),
 	dataHelper = require('../../../helpers/data.js'),
 	defaultData = requireNew('../../data/default.data.js');
 
-var data = _.merge(defaultData, {
-		meta: {
-			title: 'SocialMedia Links | WI_038',
-			description: '',
-			code: dataHelper.getTemplateCode('somelinks.hbs'),
-			documentation: dataHelper.getDocumentation('somelinks.md')
-		}
-	});
+var templateData = {
+			xingURL: 'http://www.xing.com',
+			twitterURL: 'http://www.twitter.com',
+			fbURL: 'http://www.facebook.com'
+		},
+		data = _.merge(defaultData, {
+			meta: {
+				title: 'SocialMedia Links | WI_038',
+				description: '',
+				code: dataHelper.getTemplateCode('somelinks.hbs'),
+				documentation: dataHelper.getDocumentation('somelinks.md'),
+				mocks: [
+					{
+						description: null,
+						data: dataHelper.getFormattedJSON(templateData)
+					}
+				]
+			}
+		}, templateData);
 
 module.exports = data;
