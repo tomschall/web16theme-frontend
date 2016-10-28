ec0a7d53e1c1ff1ad1ff10202c548e91a0a69996
Marcel Bührig <marcel.buehrig@zeix.com>
2016-10-28 09:46:09 +0200

    Added Arrow List example and added download list

---
+++ source/pages/listpage/listpage.data.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var _ = require('lodash'),
+	requireNew = require('require-new'),
+	defaultData = requireNew('../../data/default.data.js');
+
+var data = _.merge(defaultData, {
+		meta: {
+			title: 'Listenübersichtsseite (Download- und Linkliste)'
+		},
+		title: 'Listpage',
+		leadText: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+		widgets: {
+			header: _.assign({
+				hasPromoTeaser: false,
+				isCollapsible: true
+			}, requireNew('../../widgets/header/header.data.js')),
+			navigation: requireNew('../../widgets/navigation/navigation.data.js'),
+			footer: requireNew('../../widgets/footer/footer.data.js'),
+			hero: _.assign({
+				heroImage: '/assets/media/img/cas_eingl_mgmt_hero.png',
+				heroAlt: 'Symbolbild',
+				breadcrumb: {
+					entries: [
+						{
+							'url': '../startpage_prototype/startpage_prototype.html',
+							'title': '',
+							'extraClasses': 'is_home'
+						}, {
+							'url': '#',
+							'title': 'Weiterbildung',
+							'extraClasses': ''
+						}, {
+							'url': '.#',
+							'title': 'Fachbereiche',
+							'extraClasses': ''
+						}
+					]
+				}
+			}, requireNew('../../widgets/hero/hero.data.js')),
+			subnav: _.assign(requireNew('../../widgets/subnav/subnav.data.js'), {
+				title: 'Weiterbildung',
+				subtitle: 'Fachbereich Soziale Arbeit',
+				titleUrl: '#',
+				entries: [
+					{
+						title: 'Inhouse-Schulungen',
+						url: '#'
+					},
+					{
+						title: 'Beirat Weiterbildung',
+						url: '#'
+					},
+					{
+						title: 'Weiterbildungs-Award',
+						url: '#'
+					},
+					{
+						title: 'Zur Weiterbildungsübersicht',
+						url: '#',
+						isBack: true
+					}
+				]
+			}),
+			soMeShare: requireNew('../../widgets/so_me_share/so_me_share.data.js'),
+		}
+	});
+
+module.exports = data;
