65ef84af601590bb16aafb2104cbfe7a7a06fb1a
Marcel Bührig <marcel.buehrig@zeix.com>
2016-12-19 10:09:28 +0100

    FHNW-90

--- source/widgets/accordeon/accordeon.data.js
+++ source/widgets/accordeon/accordeon.data.js
@@ -13,7 +13,7 @@ var templateData = {
 			},
 			{
 				title: 'ICT Helpdesk',
-				accordeonContent: '<p>Es gibt im Moment in diese Mannschaft, oh, einige Spieler vergessen ihnen Profi was sie sind. Ich lese nicht sehr viele Zeitungen, aber ich habe gehört viele Situationen. Erstens: wir haben nicht offensiv gespielt. Es gibt keine deutsche Mannschaft spielt offensiv und die Name offensiv wie Bayern.Letzte Spiel hatten wir in Platz drei Spitzen: Elber, Jancka und dann Zickler. Wir müssen nicht vergessen Zickler. Zickler ist eine Spitzen mehr, Mehmet eh mehr Basler. Ist klar diese Wörter, ist möglich verstehen, was ich hab gesagt? Danke. Offensiv, offensiv ist wie machen wir in Platz. Zweitens: ich habe erklärt mit diese zwei Spieler: nach Dortmund brauchen vielleicht Halbzeit Pause. Ich habe auch andere Mannschaften gesehen in Europa nach diese Mittwoch. Ich habe gesehen auch zwei Tage die T</p>'
+				accordeonContent: '<p>Es gibt im Moment in diese Mannschaft</p>'
 			},
 			{
 				title: 'Raumvermietung',
