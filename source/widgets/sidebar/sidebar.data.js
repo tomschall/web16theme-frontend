157622bf613c19f2b56bafe8a671cb2d7a932c61
Marcel Bührig <marcel.buehrig@zeix.com>
2017-02-03 11:33:40 +0100

    Added new roles for sidebar

--- source/widgets/sidebar/sidebar.data.js
+++ source/widgets/sidebar/sidebar.data.js
@@ -16,14 +16,41 @@ var data = _.merge(defaultData, {
 			application: _.assign(requireNew('../sidebar_application/sidebar_application.data.js')),
 			events: _.assign(requireNew('../sidebar_events/sidebar_events.data.js')),
 			keydata: _.assign(requireNew('../sidebar_key_data/sidebar_key_data.data.js')),
-			contact: [
-				{
-					elementData: _.assign(requireNew('../sidebar_contact/sidebar_contact.data.js'))
-				},
-				{
-					elementData: _.assign(requireNew('../sidebar_contact/sidebar_contact.data.js'))
-				}
-			]
+			contact: _.assign(requireNew('../sidebar_contact/sidebar_contact.data.js')),
+			secretariat: _.assign(requireNew('../sidebar_contact/sidebar_contact.data.js'), {
+				title: 'Sekretariat',
+				contacts: [{
+					name: 'Laura Polexe',
+					title: 'Sekretariat',
+					url: 'http://localhost:8180/Plone/de/personen/laura-polexe',
+					telephone: '044 444 44 44',
+					jobDescr: null,
+					location: 'sdfsds',
+					email: 'laure.polexe@fhnw.ch'
+				}]
+			}),
+			degreeleadership: _.assign(requireNew('../sidebar_contact/sidebar_contact.data.js'), {
+				title: 'Degreeleadership',
+				contacts: [{
+					name: 'kontaktperson',
+					title: 'Degreeleadership',
+					url: 'http://localhost:8180/Plone/de/personen/kontaktperson',
+					telephone: '05555555',
+					location: 'Bahnhofstrasse 6\r\n5210 Windisch',
+					email: 'christina.corso@fhnw.ch'
+				}]
+			}),
+			leadership: _.assign(requireNew('../sidebar_contact/sidebar_contact.data.js'), {
+				title: 'Leadership',
+				contacts: [{
+					name: 'kontaktperson',
+					title: 'Leadership',
+					url: 'http://localhost:8180/Plone/de/personen/kontaktperson',
+					telephone: '0564620000',
+					location: 'Bahnhofstrasse 6\r\n5210 Windisch',
+					email: 'christina.corso@fhnw.ch'
+				}]
+			})
 		}
 	});
 
