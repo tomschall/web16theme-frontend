908ae98c11a2c2012ea9582080b846872d312313
Tom Gross <itconsense@gmail.com>
2017-02-15 11:53:48 +0000

    add en logo and fix whitespaces

--- source/widgets/logo/logo.data.js
+++ source/widgets/logo/logo.data.js
@@ -6,9 +6,9 @@ var _ = require('lodash'),
 	defaultData = requireNew('../../data/default.data.js');
 
 var templateData = {
-	    logo_img: '/assets/media/img/fhnw_logo_w_claim_de.svg',
+		logo_img: '/assets/media/img/fhnw_logo_w_claim_de.svg',
 		logo_url: '/',
-	    logo_alt: 'Fachhochschule Nordwestschweiz'
+		logo_alt: 'Fachhochschule Nordwestschweiz'
 	},
 	data = _.merge(defaultData, {
 		meta: {
