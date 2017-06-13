9fd8b878a76b082a5e815a8091c641a6c429a40c
Tom Gross <itconsense@gmail.com>
2017-06-13 09:00:22 +0000

    Restore DoubleStar fullbleed version

--- source/assets/js/helpers/initVendorPlugins.js
+++ source/assets/js/helpers/initVendorPlugins.js
@@ -27,6 +27,14 @@
 			align: 'top-right'
 		});
 
+		$('.widg_full_bleed_teaser img').imageScale({
+			rescaleOnResize: true,
+			scale: 'best-fill',
+			align: 'center',
+			hideParentOverflow: true,
+			fadeInDuration: true
+		});
+
 		$('.widg_teaser__img img').imageScale('scale');
 
 		if (window.estatico.mq.query({from: 'subnav'})) {
