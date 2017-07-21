e5e3176905c69124ef396c9195093468318fb0b5
Samuel Gomez <samuel.gomez@infocentric.ch>
2017-07-21 19:24:00 +0200

    Barrierefreiheit 1: Fix focusable elements become not focusable after following an anchor to them.

--- source/assets/js/helpers/links.js
+++ source/assets/js/helpers/links.js
@@ -20,11 +20,21 @@
 		$('.anchor-link').on('click.links', function(ev) {
 			ev.preventDefault();
 
+			var $targetElement;
+			var initialTabIndex;
 			if ($.attr(this, 'href') !== '#') {
+				$targetElement = $($.attr(this, 'href'));
 				$('html, body').animate({
-					scrollTop: $($.attr(this, 'href')).offset().top - 45
+					scrollTop: $targetElement.offset().top - 45
 				}, 250);
-				$($.attr(this, 'href')).attr('tabindex', 0).focus().attr('tabindex', -1);
+				initialTabIndex = $targetElement.prop('tabindex');
+				if (initialTabIndex === -1) {
+					$targetElement.prop('tabindex', 0);
+				}
+				$targetElement.focus();
+				if (initialTabIndex === -1) {
+					$targetElement.prop('tabindex', -1);
+				}
 			}
 		});
 	});
