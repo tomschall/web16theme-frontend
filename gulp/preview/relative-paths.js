38a33d307923b258e3462c4c5b1a7d07d7b1f47a
Tom Gross <itconsense@gmail.com>
2017-09-12 13:08:12 +0200

    Add support for pages

--- gulp/preview/relative-paths.js
+++ gulp/preview/relative-paths.js
@@ -7,8 +7,8 @@ var gulp = require('gulp'),
  * Makes resource paths in build folder relative
  */
 gulp.task('relative-paths', function() {
-	gulp.src('build/**/*.html').pipe(replace(/(href|src)="\/assets\//gm, function(_, attr) {
+	gulp.src('build/**/*.html').pipe(replace(/(href|src)="\/(assets|preview)\//gm, function(_, attr, dir) {
 		var relativePath = this.file.path.replace(/^.*build\//, '').split('/').map(function(f) { return ''}).join('../');
-		return [attr, '="', relativePath + 'assets/'].join('');
+		return [attr, '="', relativePath, dir, '/'].join('');
 	})).pipe(gulp.dest('build/'));
 });
