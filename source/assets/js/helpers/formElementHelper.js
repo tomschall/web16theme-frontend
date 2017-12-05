85a4115778cb528c6c69dd78afce4b34f9386fc5
wargor <webrooster@webrooster.ch>
2017-12-05 09:07:22 +0100

    Removing checked from input upload field  webteam/fhnw.webauftritt#988

--- source/assets/js/helpers/formElementHelper.js
+++ source/assets/js/helpers/formElementHelper.js
@@ -715,6 +715,12 @@
 			// workaround https://gitlab.fhnw.ch/webteam/fhnw.webauftritt/issues/988 -> removing attribute "disabled" from upload button
 			if ($('.named-file-widget input').length) {
 				$('.named-file-widget input').removeAttr('disabled');
+
+					$('.named-file-widget input').each(function(index) {
+						 if ($('.named-file-widget input').is(':checked')) {
+							 $('.named-file-widget input').removeAttr('checked');
+						 }
+					});
 			}
 		});
 	}
