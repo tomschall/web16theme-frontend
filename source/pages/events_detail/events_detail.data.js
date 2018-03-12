ad8f6375b2fcfab951a3c53fe71ad79c8b98d9f8
Samuel Gomez <samuel.gomez@infocentric.ch>
2018-03-12 12:26:42 +0100

    1097: Fix eslint errors.
    The singlequote ones and others that were trivial to fix too.

--- source/pages/events_detail/events_detail.data.js
+++ source/pages/events_detail/events_detail.data.js
@@ -9,7 +9,7 @@ var data = _.merge(defaultData, {
 		title: 'Events Detail',
 	},
 	title: 'Wirtschaftsforum',
-	event_detail: _.assign ({
+	event_detail: _.assign({
 		event_date: '1.11.2017',
 		university: 'Hochschule für Wirtschaft',
 		location_short: 'Campus Brugg-Windisch'
@@ -40,7 +40,7 @@ var data = _.merge(defaultData, {
 		header: _.assign({
 			hasPromoTeaser: false,
 			isCollapsible: true
-		},requireNew('../../widgets/header/header.data.js')),
+		}, requireNew('../../widgets/header/header.data.js')),
 		navigation: requireNew('../../widgets/navigation/navigation.data.js'),
 		footer: requireNew('../../widgets/footer/footer.data.js'),
 		hero: _.assign({
