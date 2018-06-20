b1658695dc7022ff3706b7ab4035718e4e306e3c
Karalic Slavisa <slavisa.karalic@gmail.com>
2018-06-20 13:53:49 +0200

    Adding news_sidebar templates webteam/fhnw.webauftritt#1080

---
+++ source/widgets/sidebar_news/sidebar_news.data.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var _ = require('lodash'),
+	requireNew = require('require-new'),
+	dataHelper = require('../../../helpers/data.js'),
+	defaultData = requireNew('../../data/default.data.js');
+
+var templateData = {
+			button: {
+				text: 'Alle ansehen',
+				url: '#targetInfoEvents'
+			},
+			title: 'Infoveranstaltungen',
+			entries: [
+				{
+					title: 'Info-Veranstaltung',
+					dateTime: '27.06.2016 um 18:15 Uhr',
+					location: 'Windisch',
+					url: '/'
+				},
+				{
+					title: 'Info-Veranstlatung',
+					dateTime: '27.06.2016 um 18:15 Uhr',
+					location: 'Windisch',
+					url: '/'
+				}
+			]
+		},
+		data = _.merge(defaultData, {
+			meta: {
+				title: 'Sidebar Events | WI_054',
+				description: '',
+				code: dataHelper.getTemplateCode('sidebar_events.hbs'),
+				documentation: dataHelper.getDocumentation('sidebar_events.md'),
+				mocks: [
+					{
+						description: null,
+						data: dataHelper.getFormattedJSON(templateData)
+					}
+				]
+			}
+		}, templateData);
+
+module.exports = data;
