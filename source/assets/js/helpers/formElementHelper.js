f6e1bde7f9660ab1bb744c9ea961914c36b5c28a
Tom Gross <itconsense@gmail.com>
2018-08-29 13:47:08 +0200

    readd number base

--- source/assets/js/helpers/formElementHelper.js
+++ source/assets/js/helpers/formElementHelper.js
@@ -101,7 +101,7 @@
 			if (bytes === 0) {
 					return '0 Byte';
 				}
-				var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+				var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
 				return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
 			}
 
