ab3bb0b66975d26f754156cb2008e9f820006eda
Thomas Schallert <thomas.schallert@fhnw.ch>
2018-10-30 15:43:30 +0100

    load google maps js sources async and then init location_slider to load maps
    used setTimeout for fallback cause of timing problems

--- source/assets/js/helpers/lazy.js
+++ source/assets/js/helpers/lazy.js
@@ -10,24 +10,49 @@
 
 	document.addEventListener('DOMContentLoaded', function() {
 
+		function loadScript(src, callback) {
+
+			var s,
+				r,
+				t;
+			r = false;
+			s = document.createElement('script');
+			s.type = 'text/javascript';
+			s.src = src;
+			s.onload = s.onreadystatechange = function() {
+				// console.log( this.readyState ); //uncomment this line to see which ready states are called.
+				if (!r && (!this.readyState || this.readyState === 'complete')) {
+					r = true;
+					callback();
+				}
+			};
+			t = document.getElementsByTagName('script')[0];
+			t.parentNode.insertBefore(s, t);
+
+		}
+
+		function initLocationSlider() {
+
+			var instances = Object.keys(window.estatico.widgets.location_slider.instances);
+			instances.forEach(function(inst) {
+				window.estatico.widgets.location_slider.instances[inst].init();
+			});
+		}
+
 		var lazyScripts = [].slice.call(document.querySelectorAll('script.b-lazy'));
-		console.log('lazyScripts', lazyScripts);
 
 		if ('IntersectionObserver' in window) {
 
 			var lazyScriptObserver = new IntersectionObserver(function(entries) {
-				console.log('entries:', entries);
 				entries.forEach(function(entry) {
 					if (entry.isIntersecting) {
 						var lazyScript = entry.target;
-						console.log('lazyScript: ', lazyScript);
-						lazyScript.src = lazyScript.dataset.src;
-						lazyScript.classList.remove('b-lazy');
-						lazyScript.removeAttribute('data-src');
-						lazyScript.removeAttribute('class');
-						lazyScript.removeAttribute('async');
-						lazyScriptObserver.unobserve(lazyScript);
-						console.log('successfully loaded script');
+						loadScript(lazyScript.dataset.src, function() {
+							initLocationSlider();
+							var el = document.querySelector('.b-lazy');
+							el.parentNode.removeChild(el);
+							lazyScriptObserver.unobserve(lazyScript);
+						});
 					}
 				});
 			});
@@ -40,8 +65,11 @@
 
 			window.bLazy = new window.Blazy({
 				container: '.container',
-				success: function(element) {
-					console.log('Element loaded: ', element.nodeName);
+				success: function() {
+					// console.log('Element loaded: ', element.nodeName);
+					setTimeout(function() {
+						initLocationSlider();
+					}, 500);
 				}
 			});
 
