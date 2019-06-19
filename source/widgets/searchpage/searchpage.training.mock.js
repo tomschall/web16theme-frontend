20fd0ba39cbc7d09eff6143efecc65475d29eb76
Thomas Schallert <thomas.schallert@gmail.com>
2019-06-19 13:58:30 +0200

    add portal_type FHNWEvent and fix helper function

--- source/widgets/searchpage/searchpage.training.mock.js
+++ source/widgets/searchpage/searchpage.training.mock.js
@@ -47,7 +47,7 @@ var data = {
 		{
 			'path_string': '/Plone/de/test-edu-1',
 			'description': '',
-			'Title': 'test-edu-1',
+			'Title': 'test-edu-2',
 			'start_string': '30.09.2019',
 			'portal_type': 'EduProduct',
 			'@id': 'http://localhost:8000/Plone/de/test-edu-1',
@@ -73,7 +73,7 @@ var data = {
 		{
 			'path_string': '/Plone/de/test-edu-1',
 			'description': '',
-			'Title': 'test-edu-1',
+			'Title': 'test-edu-3',
 			'start_string': 'noch ein test 30.09.2019',
 			'portal_type': 'EduProduct',
 			'@id': 'http://localhost:8000/Plone/de/test-edu-1',
@@ -101,6 +101,88 @@ var data = {
 		{
 			'path_string': '/Plone/de/test-edu-1',
 			'description': '',
+			'Title': 'test-event-1',
+			'start_string': 'test 30.09.2019',
+			'portal_type': 'FHNWEvent',
+			'@id': 'http://localhost:8000/Plone/de/test-edu-1',
+			'taxonomy_eduproducttype': [
+				'2000',
+				'2001',
+				'2008'
+			],
+			'Description': '',
+			'title': 'test-event-1',
+			'taxonomy_competence': [
+				'1001'
+			],
+			'city': [
+				'basel'
+			],
+			'taxonomy_subjectarea': [
+				'1005',
+				'1004',
+				'1000'
+			],
+			'review_state': null,
+			'@type': 'FHNWEvent'
+		},
+		{
+			'path_string': '/Plone/de/test-edu-1',
+			'description': '',
+			'Title': 'test-event-2',
+			'start_string': '30.09.2019',
+			'portal_type': 'FHNWEvent',
+			'@id': 'http://localhost:8000/Plone/de/test-edu-1',
+			'taxonomy_eduproducttype': [
+				'2002'
+			],
+			'Description': '',
+			'title': 'test-event-2',
+			'taxonomy_competence': [
+				'1001'
+			],
+			'city': [
+				'basel'
+			],
+			'taxonomy_subjectarea': [
+				'1005',
+				'1004',
+				'1000'
+			],
+			'review_state': null,
+			'@type': 'FHNWEvent'
+		},
+		{
+			'path_string': '/Plone/de/test-edu-1',
+			'description': '',
+			'Title': 'test-event-3',
+			'start_string': 'noch ein test 30.09.2019',
+			'portal_type': 'FHNWEvent',
+			'@id': 'http://localhost:8000/Plone/de/test-edu-1',
+			'taxonomy_eduproducttype': [
+				'2000',
+				'2001',
+				'2008'
+			],
+			'Description': '',
+			'title': 'test-event-3',
+			'taxonomy_competence': [
+				'1001'
+			],
+			'city': [
+				'basel'
+			],
+			'taxonomy_subjectarea': [
+				'1005',
+				'1004',
+				'1000'
+			],
+			'review_state': null,
+			'@type': 'FHNWEvent'
+		},
+		{
+			'path_string': '/Plone/de/test-edu-1',
+			'description': '',
 			'Title': 'test-edu-1',
 			'start_string': '',
 			'portal_type': 'EduProduct',
@@ -123,6 +205,32 @@ var data = {
 			],
 			'review_state': null,
 			'@type': 'EduProduct'
+		},
+		{
+			'path_string': '/Plone/de/test-edu-1',
+			'description': '',
+			'Title': 'test-event-4',
+			'start_string': '',
+			'portal_type': 'FHNWEvent',
+			'@id': 'http://localhost:8000/Plone/de/test-edu-1',
+			'taxonomy_eduproducttype': [
+				'2002'
+			],
+			'Description': '',
+			'title': 'test-event-4',
+			'taxonomy_competence': [
+				'1001'
+			],
+			'city': [
+				'basel'
+			],
+			'taxonomy_subjectarea': [
+				'1005',
+				'1004',
+				'1000'
+			],
+			'review_state': null,
+			'@type': 'FHNWEvent'
 		},
 		{
 			'path_string': '/Plone/de/test-edu-1',
