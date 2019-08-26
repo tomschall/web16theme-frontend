94617f2e4f88516823dcf254f09bc3bc7e4fea59
Roli <webrooster@webrooster.ch>
2019-08-26 17:13:22 +0200

    New logo name

--- source/widgets/logo/logo.data.js
+++ source/widgets/logo/logo.data.js
@@ -6,7 +6,7 @@ var _ = require('lodash'),
 	defaultData = requireNew('../../data/default.data.js');
 
 var templateData = {
-		logo_img: '/assets/media/img/fhnw_logo_w_claim_de.svg',
+		logo_img: '/assets/media/img/fachhochschule-nordwestschweiz-fhnw-logo.svg',
 		logo_url: '/',
 		logo_alt: 'Fachhochschule Nordwestschweiz'
 	},
