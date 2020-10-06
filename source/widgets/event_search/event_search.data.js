f6958583f8c525eed96250d7fa4197bf1d00c9a7
webrooster <roland.vonaesch@fhnw.ch>
2020-10-06 10:32:00 +0200

    EVENT SEARCH WIDGET -> new search widget

---
+++ source/widgets/event_search/event_search.data.js
@@ -0,0 +1,94 @@
+'use strict';
+
+var _ = require('lodash'),
+	requireNew = require('require-new'),
+	dataHelper = require('../../../helpers/data.js'),
+	defaultData = requireNew('../../data/default.data.js');
+
+var templateData = {
+			title: 'Event Search',
+			searchpage: _.assign(requireNew('../../widgets/searchpage/searchpage.data.js'), {
+				jsonURL: '/mocks/widgets/searchpage/searchpage.category.json'
+			}),
+			langStrings: {
+				label_search: 'Suchbegriff',
+				label_btn_search: 'Suchen',
+				label_reset_fields: 'Reset',
+				label_location: 'Ort',
+				label_school: 'Hochschule',
+				label_type: 'Typ',
+			},
+			facultyOptions: [
+				{
+					'optionLabel': 'Architektur, Bau & Geomatik',
+					'optionValue': '1000'
+				},
+				{
+					'optionLabel': 'International Studies',
+					'optionValue': '1003',
+					'selected': 'selected'
+				},
+				{
+					'optionLabel': 'Life Sciences',
+					'optionValue': '1004'
+				},
+				{
+					'optionLabel': 'Musik',
+					'optionValue': '1005'
+				},
+			],
+			eventTypeOptions: [
+				{
+					'optionLabel': 'Info-Anlass',
+					'optionValue': '2008'
+				},
+				{
+					'optionLabel': 'Ringvorlesung',
+					'optionValue': '2000',
+					'selected': 'selected'
+				},
+				{
+					'optionLabel': 'Another Event',
+					'optionValue': '2001'
+				}
+			],
+
+			locationOptions: [
+				{
+					'optionLabel': 'Basel',
+					'optionValue': 'basel'
+				},
+				{
+					'optionLabel': 'Muttenz',
+					'optionValue': 'muttenz'
+				},
+				{
+					'optionLabel': 'Brugg-Windisch',
+					'optionValue': 'brugg'
+				},
+				{
+					'optionLabel': 'Olten',
+					'optionValue': 'olten'
+				},
+				{
+					'optionLabel': 'Solothurn',
+					'optionValue': 'solothurn'
+				}
+			],
+		},
+		data = _.merge(defaultData, {
+			meta: {
+				title: 'Event Search | WI_073',
+				description: '',
+				code: dataHelper.getTemplateCode('event_search.hbs'),
+				documentation: dataHelper.getDocumentation('event_search.md'),
+				mocks: [
+					{
+						description: null,
+						data: dataHelper.getFormattedJSON(templateData)
+					}
+				]
+			}
+		}, templateData);
+
+module.exports = data;
---
+++ source/widgets/event_search/event_search.md
@@ -0,0 +1,12 @@
+## Description
+
+In-Content-Suche ein Suchmodul das im Content eingebunden werden kann (nicht Suchleiste, nicht eigene Suchseite)
+
+## Integration
+
+HTML einbinden. Optionen und Felder werden via HTML gesetzt nicht via data.js
+
+### Fields
+
+* **title**: Titel für das Widget
+* **searchFilterPageURL**: URL zur spezifischen Suchseite (z.B. Weiterbildungssuche)
