3a4206fd1a55e7212729cda432aaf642b0a42ebb
webrooster <roland.vonaesch@fhnw.ch>
2021-01-05 11:30:11 +0100

    Table mobile scroll icon

--- source/assets/js/helpers/tables.js
+++ source/assets/js/helpers/tables.js
@@ -28,25 +28,27 @@
 	});
 
 	if (/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-		$('.mobile__scroll, .striped__mobile_scroll').on('touchstart, ontouchstart', function() {
-			$(this).addClass('touched');
+		$('table').on('touchstart, ontouchstart', function() {
+			$(this).addClass('touched, mobile__scroll');
 		});	
 	}
-
-	$('.mobile__scroll, .mobile__scroll_striped').on('scroll click', function() {
+	
+	$('table').on('scroll click', function() {
 		$(this).addClass('scrolling');
 	});
 
 	// REMOVE SCROLL ICON IF TABLE IS NOT SCROLLABLE AND SMALLER THAN CONTENT COLUMN
 	// webteam/fhnw.webauftritt#1580
-	$('table.mobile__scroll tbody').each(function() {
+	$('table tbody').each(function() {
 		var tableWidth = $(this).outerWidth();
 		var contentElem = document.querySelector('.content__element');
 		var rectContentElement = contentElem.getBoundingClientRect();
 		var contentWidth = rectContentElement.width;
 		console.log(tableWidth, contentWidth);
 		if (tableWidth < contentWidth) {
-				$(this).parent().removeClass('mobile__scroll');
+				$(this).parent().removeClass('scrolling, mobile__scroll');
+		} else {
+			$(this).parent().addClass('scrolling, mobile__scroll');
 		}
  });
 
