bdc41265ae20dd795401febd66961d0c78e97e21
Thomas Schallert <thomas.schallert@fhnw.ch>
2021-02-24 10:29:16 +0100

    fix deps

--- prepare_build_theme.sh
+++ prepare_build_theme.sh
@@ -4,8 +4,8 @@ source bin/activate
 bin/pip install -r requirements.txt
 bin/buildout
 pip install nodeenv # then install nodeenv (nodeenv==0.7.1 was installed)
-nodeenv --python-virtualenv # Use current python virtualenv
+nodeenv --python-virtualenv --node=11.15.0 # Use current python virtualenv
 npm install -g yarn # install lessc in the virtualenv
 echo "Current Node Version"
 node --version
-bin/yarn install
+yarn install
