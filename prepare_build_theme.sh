3c33956d2a4887d2c61d06924a3cf01354567dbc
Thomas Schallert <thomas.schallert@fhnw.ch>
2021-02-24 10:55:08 +0100

    fix deps

--- prepare_build_theme.sh
+++ prepare_build_theme.sh
@@ -5,10 +5,13 @@ bin/pip install -r requirements.txt
 bin/buildout
 pip install nodeenv
 nodeenv --python-virtualenv --node=11.15.0
+ln -s bin/node /usr/bin/env/node
 npm install -g yarn gulp@3.9.1
 echo "Current node version"
 node --version
 echo "Show bin directory"
 ls bin/
+echo "Show directory"
+ls -al
 echo "Install packages with yarn"
 yarn install
