59c87ca7007446cd5648b8399d218d5285cbf9b6
Thomas Schallert <thomas.schallert@fhnw.ch>
2021-03-03 16:07:35 +0100

    fix image scale

--- source/assets/js/helpers/initVendorPlugins.js
+++ source/assets/js/helpers/initVendorPlugins.js
@@ -4,37 +4,31 @@
  **/
 
 ;(function($, undefined) {
-	'use strict';
+  'use strict';
 
-	if ($('.widg_teaser__img img').length) {
-		$(window).resize(function() {
-			if ($(window).width() > 1023) {
-				$('.widg_teaser__img img').imageScale({
-					rescaleOnResize: true,
-					scale: 'best-fill',
-					align: 'center'
-				});
-			}
-		});
-	}
+  jQuery(function() {
 
-	$(document).ready(function() {
+    var scale = function(el) {
+      $(el).imageScale({
+        rescaleOnResize: true,
+        scale: 'best-fill',
+        align: 'center'
+      });
+    };
+
+    if ($('.widg_teaser__img img').length) {
+      $(window).on('resize', function() {
+        if ($(window).width() > 1023) {
+          scale($('.widg_teaser__img img'));
+        }
+      });
+    }
 		// The Jquery Spinner, has to be implemented when element is added later than document.ready
 		$('.fhnw-spinner').spinner({
 			radius: 30,
 			strokeWidth: 6,
 			color: '#999999'
 		});
-
-		if ($('.widg_teaser__img img').length) {
-			if ($(window).width() > 1023) {
-				$('.widg_teaser__img img').imageScale({
-					rescaleOnResize: true,
-					scale: 'best-fill',
-					align: 'center'
-				});
-			}
-		}
 
 		function initScale(el) {
 			if ($(el).data('imageScale')) {
@@ -44,17 +38,13 @@
 			$(el).parent()
 				.css('height', maxHeight + 'px')
 				.addClass('is-loaded');
-			$(el).imageScale({
-				rescaleOnResize: true,
-				scale: 'best-fill',
-				align: 'center'
-			});
+      scale($(el));
 		}
 
 		if ($('.widg_teaser__img img').length) {
 			if ($(window).width() > 1023) {
-				$('.widg_teaser__img img').imageScale(
-					initScale(this)
+        $('.widg_teaser__img img').imageScale(
+          initScale(this)
 				);
 			}
 		}
