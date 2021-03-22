9c178813ffe937fe79cd70b5d7174fb1cf142c37
webrooster <roland.vonaesch@fhnw.ch>
2021-03-22 10:42:26 +0100

    Include deleted scripts for 1/3 1/4 teasers

--- source/assets/js/helpers/initVendorPlugins.js
+++ source/assets/js/helpers/initVendorPlugins.js
@@ -3,16 +3,70 @@
 
  **/
 
-;(function($, undefined) {
-  'use strict';
+ ;(function($, undefined) {
+	'use strict';
 
-  jQuery(function() {
+	$(window).resize(function() {
+		if ($(window).width() > 1023) {
+			$('.widg_teaser__img img').imageScale({
+				rescaleOnResize: true,
+				scale: 'best-fill',
+				align: 'center'
+			});
+		}
+	});
+
+	$(document).ready(function() {
 		// The Jquery Spinner, has to be implemented when element is added later than document.ready
 		$('.fhnw-spinner').spinner({
 			radius: 30,
 			strokeWidth: 6,
 			color: '#999999'
 		});
+
+		if ($(window).width() > 1023) {
+			$('.widg_teaser__img img').imageScale({
+				rescaleOnResize: true,
+				scale: 'best-fill',
+				align: 'center'
+			});
+		}
+
+		function initScale(el) {
+			if ($(el).data('imageScale')) {
+				return;
+			}
+			var maxHeight = Math.min((el.naturalHeight || el.height), 526);
+			$(el).parent()
+				.css('height', maxHeight + 'px')
+				.addClass('is-loaded');
+			$(el).imageScale({
+				rescaleOnResize: true,
+				scale: 'best-fill',
+				align: 'center'
+			});
+		}
+
+		if ($(window).width() > 770) {
+			$('.widg_full_bleed_teaser img').load(function() {
+				initScale(this);
+			}).each(function() {
+				if (this.complete) {
+					// already loaded before the load event got registered
+					initScale(this);
+				}
+			});
+		}
+
+
+		if ($(window).width() > 1023) {
+			$('.widg_teaser__img img').imageScale({
+				rescaleOnResize: true,
+				scale: 'best-fill',
+				align: 'center'
+			});
+		}
+
 
 		if (window.estatico.mq.query({from: 'subnav'})) {
 			$('.search__form-wrapper').scrollToFixed();
