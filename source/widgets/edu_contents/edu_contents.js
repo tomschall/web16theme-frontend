1cfe82c35b3e0c8b6f0503c6780375fc7645f82b
webrooster <roland.vonaesch@fhnw.ch>
2021-05-21 13:45:25 +0200

    EDU-Product - Mobile Anchor Navigation

--- source/widgets/edu_contents/edu_contents.js
+++ source/widgets/edu_contents/edu_contents.js
@@ -60,19 +60,48 @@
 			if ($('.widg_edu_contents .edu__key_data div.datas').length > 3) {
 				$('.edu__key_data div:gt(2)').hide();
 			}
-
 			$('.show-more').on('click', function() {
 				$('.edu__key_data div.datas:gt(2)').toggle();
 				// eslint-disable-next-line no-unused-expressions
-				$(this).text() === 'Mehr anzeigen'
-					? $(this).text('Weniger anzeigen')
-					: $(this).text('Mehr anzeigen');
+				$(this).text() === 'Mehr anzeigen' ? $(this).text('Weniger anzeigen') : $(this).text('Mehr anzeigen');
 				// eslint-disable-next-line no-unused-expressions
-				$(this).text() === 'Weniger anzeigen'
-					? $(this).addClass('open')
-					: $(this).removeClass('open');
+				$(this).text() === 'Weniger anzeigen' ? $(this).addClass('open') : $(this).removeClass('open');
+			});
+			this.mobileNavigation();
+		}
+	};
+
+	Widget.prototype.mobileNavigation = function() {
+		if (estatico.mq.query({ to: 'small' })) {
+			var e = $('<div><div class="icon icon__contact"></div><div class="icon icon__info"></div><div class="icon icon__application"></div></div>');
+			$('body').append(e);
+			e.attr('id', 'edu__product_nav');
+		}
+		// ANCHOR CONTACT
+		if ($('#edu__contact').length) {
+			$('.icon.icon__contact').on('click', function() {
+				$('html, body').animate({
+						scrollTop: $('.widg_sidebar__content').offset().top - 0
+				}, 1000);
 			});
 		}
+		// ANCHOR EVENTS
+		if ($('#edu__events').length) {
+			$('.icon.icon__info').on('click', function() {
+				$('html, body').animate({
+						scrollTop: $('.widg_edu_events.widg_sidebar__object').offset().top - 10
+				}, 1000);
+			});
+		}
+		// ANCHOR APPLICATION
+		if ($('#edu__application').length) {
+			$('.icon.icon__application').on('click', function() {
+				$('html, body').animate({
+						scrollTop: $('#edu__application').offset().top - 25
+				}, 1000);
+			});
+		}
+
 	};
 
 	/**
