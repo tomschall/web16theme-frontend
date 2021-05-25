efe2e02e979d823dec0c927c71ab6ae383f150c6
Thomas Schallert <thomas.schallert@fhnw.ch>
2021-05-25 15:50:15 +0200

    webteam/fhnw.webauftritt#1754 - add spitzmarken

--- source/widgets/extendedlinks/extendedlinks.data.js
+++ source/widgets/extendedlinks/extendedlinks.data.js
@@ -12,11 +12,13 @@ var templateData = {
 		{
 			title: 'Die neun Hochschulen der FHNW',
 			description: 'Die Fachhochschule Nordwestschweiz FHNW umfasst neun Hochschulen, die auf die Hauptstandorte Aarau, Basel, Brugg/Windisch, Muttenz und Olten konzentriert sind.',
-			url: '../universities_overview/universities_overview.html'
+			dateline: 'CAS',
+      url: '../universities_overview/universities_overview.html'
 		}, {
 			title: 'Bibliotheken der FHNW',
 			description: 'Die Fachhochschule Nordwestschweiz FHNW verfügt an ihren verschiedenen Standorten über zahlreiche Bibliotheken',
-			url: '../library_overview/library_overview.html',
+			dateline: 'CAS',
+      url: '../library_overview/library_overview.html',
 					news_detail: {
 						news_date: '27.06.2017',
 						university: 'Hochschule für angewandte Psychologie'
