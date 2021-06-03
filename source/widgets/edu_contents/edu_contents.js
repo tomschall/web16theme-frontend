df63cadc5463f5222a7f2e3a820c495e2191e4a0
webrooster <roland.vonaesch@fhnw.ch>
2021-06-03 15:06:59 +0200

    EDU-Product - switching id to class, js DRY

--- source/widgets/edu_contents/edu_contents.js
+++ source/widgets/edu_contents/edu_contents.js
@@ -15,7 +15,16 @@
 			// eventname: 'eventname.estatico.' + name
 		},
 		defaults = {
-			domSelectors: {},
+			domSelectors: {
+				parent: '.widg_edu_contents',
+				contact: '.edu__contact',
+				application: '#edu__application',
+				events: '#edu__events',
+				topLink: '.widg_toplink',
+				contactAnchor: '.widg_sidebar__content',
+				eventAnchor: '.widg_edu_events.widg_sidebar__object',
+				applicationAnchor: '#edu__application'
+			},
 			stateClasses: {},
 		},
 		data = {
@@ -49,9 +58,9 @@
 	 * @public
 	 */
 	Widget.prototype.init = function() {
-		var labelApplication = $('.widg_edu_contents').attr('data-nav-application');
-		var labelEvents = $('.widg_edu_contents').attr('data-nav-events');
-		var labelContact = $('.widg_edu_contents').attr('data-nav-contact');
+		var labelApplication = $(this.options.domSelectors.parent).attr('data-nav-application');
+		var labelEvents = $(this.options.domSelectors.parent).attr('data-nav-events');
+		var labelContact = $(this.options.domSelectors.parent).attr('data-nav-contact');
 
 		if (window.estatico.mq.query({ to: 'small' })) {
 			var e = $('<div><div class="icon icon__application">' + labelApplication +
@@ -62,44 +71,32 @@
 			e.attr('id', 'edu__product_nav').hide().fadeIn(1000);
 		}
 		// ANCHOR CONTACT
-		if ($('#edu__contact').length) {
+		if ($(this.options.domSelectors.contactAnchor).length) {
 			$('.icon.icon__contact').on('click', function() {
-				$('html, body').animate(
-					{
-						scrollTop: $('.widg_sidebar__content').offset().top - 0,
-					},
-					1000
-				);
-			});
+				Widget.prototype.scrollTop(defaults.domSelectors.contactAnchor, 0);
+			}).bind(this);
 		}
 		// ANCHOR EVENTS
-		if ($('#edu__events').length) {
+		if ($(this.options.domSelectors.events).length) {
 			$('.icon.icon__info').on('click', function() {
-				$('html, body').animate(
-					{
-						scrollTop:
-							$('.widg_edu_events.widg_sidebar__object').offset().top - 10,
-					},
-					1000
-				);
+				Widget.prototype.scrollTop(defaults.domSelectors.eventAnchor, 10);
 			});
 		}
 		// ANCHOR APPLICATION
-		if ($('#edu__application').length) {
+		if ($(this.options.domSelectors.application).length) {
 			$('.icon.icon__application').on('click', function() {
-				$('html, body').animate(
-					{
-						scrollTop: $('#edu__application').offset().top - 25,
-					},
-					1000
-				);
+				Widget.prototype.scrollTop(defaults.domSelectors.applicationAnchor, 25);
 			});
 		}
 
 		// REPLACING BACK-TO-TOP-LINK
-		if ($('.widg_toplink').length) {
-			$('.widg_toplink').css('bottom', '90px');
+		if ($(this.options.domSelectors.topLink).length) {
+			$(this.options.domSelectors.topLink).css('bottom', '90px');
 		}
+	};
+
+	Widget.prototype.scrollTop = function(selector, param) {
+		$('html, body').animate({ scrollTop: $(selector).offset().top - param }, 1000 );
 	};
 
 	/**
