a2ee65778c149423a3fa5646f143b32c18fbfe0f
Thomas Schallert <thomas.schallert@fhnw.ch>
2021-11-15 13:29:30 +0100

    add svelte specific stuff to estatico

--- source/assets/svelte/src/main.ts
+++ source/assets/svelte/src/main.ts
@@ -1,13 +1,13 @@
 import SearchBar from './SearchBar.svelte';
 import App from './App.svelte';
 
-const searchbar = new SearchBar({
-  target: document.querySelector('.widg_searchbar-bar'),
-});
-
 const app = new App({
   target: document.body,
   props: { name: 'world' },
+});
+
+const searchbar = new SearchBar({
+  target: document.querySelector('.widg_searchbar-bar'),
 });
 
 export default searchbar;
