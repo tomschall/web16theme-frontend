1bdd19f464922a93dc953e550a51630efd981cc3
wargor <webrooster@webrooster.ch>
2021-11-18 17:21:59 +0100

    Svelte Search - no results transition (TBD)

--- source/assets/svelte/src/SearchResults.svelte
+++ source/assets/svelte/src/SearchResults.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { _ } from 'svelte-i18n';
+	import { fly } from 'svelte/transition';
 	import ListItem from './ListingNormal.svelte';
 	export let results;
 	export let activeSearch;
@@ -7,7 +8,7 @@
 
 <div class="search__results">
 	{#if activeSearch === true}
-		<div class="no__results">
+		<div class="no__results" transition:fly="{{ y: -200, duration: 2000 }}">
 			{$_('search_no_results')}
 		</div>
 	{/if}
