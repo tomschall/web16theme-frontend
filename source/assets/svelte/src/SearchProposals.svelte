3a4c35a5e5df0af6cb6a77261b421504d337dd0d
wargor <webrooster@webrooster.ch>
2021-11-23 08:30:30 +0100

    Svelte Search - proposal link (TBD)

--- source/assets/svelte/src/SearchProposals.svelte
+++ source/assets/svelte/src/SearchProposals.svelte
@@ -1,23 +1,26 @@
 <script>
-  import { _ } from 'svelte-i18n';
-  import SvelteMarkdown from 'svelte-markdown';
+	import { _ } from 'svelte-i18n';
+	import SvelteMarkdown from 'svelte-markdown';
 
 	let searchProposals = [
-			'Lorem **ipsum**',
-      'Lorem ipsum **dolor**',
-      'Lorem ipsum dolor **sit**',
-      'Lorem ipsum dolor sit **amet**',
-    ];
+		'Lorem **ipsum**',
+		'Lorem ipsum **dolor**',
+		'Lorem ipsum dolor **sit**',
+		'Lorem ipsum dolor sit **amet**',
+	];
 
 </script>
 
 <div class="widg_search_proposals">
-  <div class="widg_searchbar-bar__title">
-    <p>{$_('search_proposal_title')}</p>
-  </div>
-  <ul>
-    {#each searchProposals as source (source)}
-      <li><SvelteMarkdown {source} /></li>
-    {/each}
-  </ul>
+	<div class="widg_searchbar-bar__title">
+		<p>{$_('search_proposal_title')}</p>
+	</div>
+	<ul>
+		{#each searchProposals as source (source)}
+			<li>
+				<a href="#"><SvelteMarkdown {source} /></a
+				>
+			</li>
+		{/each}
+	</ul>
 </div>
