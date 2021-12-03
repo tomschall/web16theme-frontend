8b0b1a0c0b5133e99af194150f7713eb2440c47a
wargor <webrooster@webrooster.ch>
2021-12-03 11:13:58 +0100

    Svelte Searchpage - update template

--- source/pages/search_all/search_all.data.js
+++ source/pages/search_all/search_all.data.js
@@ -14,10 +14,70 @@ var data = _.merge(defaultData, {
 				hasPromoTeaser: false,
 				isCollapsible: false
 			}, requireNew('../../widgets/header/header.data.js')),
+			hero: _.assign(
+				{
+					heroImage: '/assets/media/img/panorama.jpg',
+					heroAlt: 'Symbolbild',
+					breadcrumb: {
+						entries: [
+							{
+								url: '../startpage_prototype/startpage_prototype.html',
+								title: '',
+								extraClasses: 'is_home',
+							},
+							{
+								url: '#',
+								title: 'Die FHNW',
+								extraClasses: '',
+							},
+							{
+								url: '#',
+								title: 'Suche',
+								extraClasses: '',
+							},
+						],
+					},
+				},
+				requireNew('../../widgets/hero/hero.data.js')
+			),
 			navigation: requireNew('../../widgets/navigation/navigation.data.js'),
 			footer: requireNew('../../widgets/footer/footer.data.js'),
-			searchpage: _.assign(requireNew('../../widgets/searchpage/searchpage.data.js'), {
-				jsonURL: '/mocks/widgets/searchpage/searchpage.json'
+			subnav: _.assign(requireNew('../../widgets/subnav/subnav.data.js'), {
+				title: 'Die FHNW',
+				subtitle: 'Suchen',
+				titleUrl: '#',
+				entries: [
+					{
+						title: 'Studium suchen',
+						url: '#',
+						isBack: false,
+					},
+					{
+						title: 'Weiterbildungen und Kurse suchen',
+						url: '#',
+						isBack: false,
+					},
+					{
+						title: 'Personen suchen',
+						url: '#',
+						isBack: false,
+					},
+					{
+						title: 'Veranstaltungen suchen',
+						url: '#',
+						isBack: false,
+					},
+					{
+						title: 'News suchen',
+						url: '#',
+						isBack: false,
+					},
+					{
+						title: 'Startseite',
+						url: '#',
+						isBack: true,
+					},
+				],
 			}),
 			actionbuttons: requireNew('../../widgets/actionbuttons/actionbuttons.data.js'),
 
