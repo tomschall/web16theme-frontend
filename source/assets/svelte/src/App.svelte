76557295d5fd2d8a0252260b27909d1315614b37
Thomas Schallert <thomas.schallert@fhnw.ch>
2021-12-03 10:56:14 +0100

    remove unused components

--- source/assets/svelte/src/App.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import Content from './modal/Content.svelte';
-	import Modal from './modal/Modal.svelte';
-	import { modal } from './stores.js';
-</script>
-
-<Modal show={$modal}>
-	<Content />
-</Modal>
