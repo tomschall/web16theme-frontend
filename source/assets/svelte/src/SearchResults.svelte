cf8293fe304f366a2688923e810f85045056f929
Roland Von Aesch <roland.vonaesch@fhnw.ch>
2021-12-10 16:00:03 +0100

    Update SearchResults.svelte -> remove window.location.host

--- source/assets/svelte/src/SearchResults.svelte
+++ source/assets/svelte/src/SearchResults.svelte
@@ -25,7 +25,7 @@
 		{#if results.length > 0 && template === 'searchbar'}
 			<a
 				class="widg_searchbar__go-to-page not-default"
-				href={`${window.location.host}/search_all?query=${searchTerm}&searchtype=${searchType}`}
+				href={`${/search_all?query=${searchTerm}&searchtype=${searchType}`}
 				>{$_('search_all_results')}</a
 			>
 		{/if}
