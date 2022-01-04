8b8788c43cb01c2050811ab3d10b28d19c52194d
wargor <webrooster@webrooster.ch>
2022-01-04 16:17:48 +0100

    Svelte edu search - form labels

--- source/assets/svelte/src/ContinuingEducationSearch.svelte
+++ source/assets/svelte/src/ContinuingEducationSearch.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	console.log('CONTINUNING EDUCATION FORM');
+	import { _ } from 'svelte-i18n';
 </script>
 
 <div class="search__wrapper">
@@ -15,7 +15,9 @@
 						data-searchpage="query"
 						data-searchparam="q"
 					/>
-					<label class="formQuestion" for="search-string">LABEL SEARCH</label>
+					<label class="formQuestion" for="search-string"
+						>{$_('edu_search_label')}</label
+					>
 				</div>
 			</div>
 		</div>
@@ -26,13 +28,15 @@
 					name="taxonomy_subjectarea"
 					data-searchparam="taxonomy_subjectarea"
 					class="custom-select"
-					multiple="multiple"
+					multiple={true}
 				>
 					<option value="1000">Option I</option>
 					<option value="1001">Option II</option>
 					<option value="1002">Option III</option>
 				</select>
-				<label class="formQuestion" for="faculty">Label Faculty</label>
+				<label class="formQuestion" for="faculty"
+					>{$_('edu_faculty_label')}</label
+				>
 				<span class="custom-select___remover" aria-hidden="true" />
 			</div>
 
@@ -46,12 +50,14 @@
 					id="taxonomy_eduproducttype"
 					name="taxonomy_eduproducttype"
 					data-searchparam="taxonomy_eduproducttype"
-					multiple="multiple"
+					multiple={true}
 				>
 					<option value="education_type_1">Education Type I</option>
 					<option value="education_type_2">Education Type II</option>
 				</select>
-				<label class="formQuestion" for="study_type">Label Study</label>
+				<label class="formQuestion" for="study_type"
+					>{$_('edu_study_type_label')}</label
+				>
 				<span class="custom-select___remover" aria-hidden="true" />
 			</div>
 			<div class="search__holder select2__wrapper">
@@ -60,12 +66,14 @@
 					name="city"
 					id="city"
 					data-searchparam="city"
-					multiple="multiple"
+					multiple={true}
 				>
 					<option value="option_study_1">Study I</option>
 					<option value="option_study_2">Study II</option>
 				</select>
-				<label class="formQuestion" for="location">Label Location</label>
+				<label class="formQuestion" for="location"
+					>{$_('edu_location_label')}</label
+				>
 				<span class="custom-select___remover" aria-hidden="true" />
 			</div>
 		</div>
@@ -73,12 +81,12 @@
 		<div class="search__third-row">
 			<div class="search__extra-holder">
 				<button class="search__reset not-default" data-searchpage="reset"
-					>Reset</button
+					>{$_('edu_form_reset')}</button
 				>
-			</div>
-			<div class="search__extra-holder element-hidden">
-				<span data-searchpage="countNumber" />&nbsp;
-				<span class="search__countNumber___text" />
+				<div class="listing__type">
+					<span>List</span>
+					<span>Grid</span>
+				</div>
 			</div>
 		</div>
 	</div>
