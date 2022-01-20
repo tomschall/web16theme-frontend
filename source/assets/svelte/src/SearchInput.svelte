1ad126958853d8784127372bbaffce2feacf484c
wargor <webrooster@webrooster.ch>
2022-01-20 14:22:24 +0100

    Mulitselect - search field label animation

--- source/assets/svelte/src/SearchInput.svelte
+++ source/assets/svelte/src/SearchInput.svelte
@@ -10,9 +10,11 @@
 	export let searchTermSpellCheck: string;
 	export let searchType: string;
 	export let handleInput: () => void;
+	export let toggleState: boolean = false;
+
 	let ref = null;
 
-	onMount(() => ref.focus());
+	// onMount(() => ref.focus());
 
 	const handleClick = () => {
 		query = '';
@@ -21,6 +23,18 @@
 		showStatusInfo = false;
 		searchTermSpellCheck = null;
 		searchType = 'continuing_education';
+		toggleState = false;
+	};
+
+	const toggleLabel = () => {
+		if (toggleState === false) {
+			toggleState = true;
+			ref.focus();
+		} else if (query.length === 0) {
+			toggleState = false;
+		} else if (query.length > 0) {
+			toggleState = true;
+		}
 	};
 </script>
 
@@ -28,15 +42,21 @@
 	<div class="search__string">
 		<form autocomplete="off" on:submit|preventDefault>
 			<input
+				style={toggleState === true ? 'padding-top: 30px' : ''}
 				bind:value={query}
 				bind:this={ref}
 				on:input={handleInput}
+				on:input={toggleLabel}
+				on:click={toggleLabel}
+				on:blur={toggleLabel}
 				type="text"
 				name="searchbar_search"
 				data-searchbar="input"
 			/>
-			<label for="search" class="searchbar_search"
-				>{$_('search_placeholder')}</label
+			<label
+				for="search"
+				class="searchbar_search {toggleState === true ? 'toggle__top' : ''}"
+				on:click={toggleLabel}>{$_('multiple_label_search')}</label
 			>
 			<a on:click={handleClick} href={void 0} class="search__string__clear"
 				><i /></a
