f068504a54bda76a10ccc2fbb16b66fe324e95a4
Schulz Adrian <adrian.schulz@fhnw.ch>
2022-01-27 16:31:50 +0100

    Added mock-data for continuing_education_search.

--- source/widgets/continuing_education_search/continuing_education_search.data.js
+++ source/widgets/continuing_education_search/continuing_education_search.data.js
@@ -5,20 +5,38 @@ var _ = require('lodash'),
 	dataHelper = require('../../../helpers/data.js'),
 	defaultData = requireNew('../../data/default.data.js');
 
-var templateData = {},
-		data = _.merge(defaultData, {
+var templateData = {
+		widget_data:
+			'{"filterSubjectarea": false, "filterEduproducttype": false, "filterLocation": false, ' +
+			'"subjectareaOptions": [{"optionValue": "1003", "optionLabel": "International Studies", "selected": false}, ' +
+			'{"optionValue": "1009", "optionLabel": "Technik", "selected": false}], "eduproducttypeOptions": [{"optionValue": "8wjt11bpvt", ' +
+			'"optionLabel": "Ausbildungen", "selected": false}, {"optionValue": "2000", "optionLabel": "CAS", "selected": false}, ' +
+			'{"optionValue": "2001", "optionLabel": "DAS", "selected": false}, {"optionValue": "2015", "optionLabel": "Studierendenseminar",' +
+			' "selected": false}, {"optionValue": "2018", "optionLabel": "Tagung", "selected": false}, {"optionValue": "kr5y78pjyt",' +
+			' "optionLabel": "Weiterbildungen", "selected": false, "hide": true}], "locationOptions": [{"optionValue": "crrsav5xc4",' +
+			' "optionLabel": "Andere", "selected": false}, {"optionValue": "basel", "optionLabel": "Basel", "selected": false}, ' +
+			'{"optionValue": "muttenz", "optionLabel": "Muttenz", "selected": false}], "subject": [],' +
+			' "jsonURL": "http://localhost:8000/Plone/de/searchbar.json", "filterURL": "http://localhost:8000/Plone/de/searchbar.json"}',
+	},
+	data = _.merge(
+		defaultData,
+		{
 			meta: {
 				title: 'Continuing Education Search | WI_076',
 				description: '',
 				code: dataHelper.getTemplateCode('continuing_education_search.hbs'),
-				documentation: dataHelper.getDocumentation('continuing_education_search.md'),
+				documentation: dataHelper.getDocumentation(
+					'continuing_education_search.md'
+				),
 				mocks: [
 					{
 						description: null,
-						data: dataHelper.getFormattedJSON(templateData)
-					}
-				]
-			}
-		}, templateData);
+						data: dataHelper.getFormattedJSON(templateData),
+					},
+				],
+			},
+		},
+		templateData
+	);
 
 module.exports = data;
