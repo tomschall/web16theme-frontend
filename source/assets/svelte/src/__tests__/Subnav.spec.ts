02190baae523639d497320c804c26960e12991ea
Thomas Schallert <thomas.schallert@fhnw.ch>
2022-02-12 17:57:53 +0100

    add a test for Subnav component

---
+++ source/assets/svelte/src/__tests__/Subnav.spec.ts
@@ -0,0 +1,62 @@
+import { render, screen } from '@testing-library/svelte';
+import TestComponent from '../TestComponent.svelte';
+import Subnav from '../Subnav.svelte';
+import en from '../lang/en.json';
+import de from '../lang/de.json';
+import { init, addMessages, _ } from 'svelte-i18n';
+import util from 'util';
+import prettyFormat from 'pretty-format';
+
+let lang: string = document.documentElement.lang;
+
+export const format = (value: unknown): string =>
+  typeof value === 'function'
+    ? value.toString()
+    : prettyFormat(value, { min: true });
+
+// jest.mock('svelte-i18n', () => {
+//   const originalModule = jest.requireActual('svelte-i18n');
+
+//   return {
+//     __esModule: true,
+//     ...originalModule,
+//     default: jest.fn(() => 'mocked baz'),
+//     addMessages,
+//     init,
+//     _,
+//   };
+// });
+
+addMessages('en', en);
+addMessages('de', de);
+
+init({
+  fallbackLocale: 'de',
+  initialLocale: lang,
+});
+
+test('render subnav', () => {
+  const { getByText } = render(Subnav);
+
+  expect(getByText('Die FHNW'));
+
+  expect(getByText('Suchen'));
+
+  const links = [
+    '/de/die-fhnw',
+    '/de/search_all',
+    '/de/search_edu',
+    '/de/search_filter',
+    '/de/search_profiles',
+    '/de/die-fhnw/veranstaltungen',
+    '/de/medien/newsroom/news',
+    '/de',
+  ];
+
+  document
+    .querySelectorAll('a')
+    .forEach((el, i) => expect(el.getAttribute('href')).toBe(links[i]));
+
+  const myObject = document.querySelectorAll('a')[5].getAttribute('href');
+  console.log(prettyFormat(myObject));
+});
--- source/assets/svelte/src/__tests__/TestComponent.spec.ts
+++ source/assets/svelte/src/__tests__/TestComponent.spec.ts
@@ -1,32 +1,5 @@
 import { render, screen } from '@testing-library/svelte';
 import TestComponent from '../TestComponent.svelte';
-import Subnav from '../Subnav.svelte';
-import en from '../lang/en.json';
-import de from '../lang/de.json';
-import { init, addMessages, _ } from 'svelte-i18n';
-
-let lang: string = document.documentElement.lang;
-
-// jest.mock('svelte-i18n', () => {
-//   const originalModule = jest.requireActual('svelte-i18n');
-
-//   return {
-//     __esModule: true,
-//     ...originalModule,
-//     default: jest.fn(() => 'mocked baz'),
-//     addMessages,
-//     init,
-//     _,
-//   };
-// });
-
-addMessages('en', en);
-addMessages('de', de);
-
-init({
-  fallbackLocale: 'de',
-  initialLocale: lang,
-});
 
 test("find text 'Studium suchen'", () => {
   render(TestComponent);
@@ -40,11 +13,6 @@ test("find alt text attribute 'sindbad'", () => {
   const node = screen.findAllByAltText('sindbad');
   expect(node).not.toBeNull();
 
-  const node2: HTMLElement = screen.queryByText('de');
+  const node2: HTMLElement = screen.queryByText('en');
   expect(node2).not.toBeNull();
-});
-
-test('render subnav', () => {
-  const { getByText } = render(Subnav);
-  expect(getByText('Die FHNW'));
 });
