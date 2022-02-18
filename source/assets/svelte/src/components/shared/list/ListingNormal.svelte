6f95ac37cae7ea2681471928ab557c8a464c23bf
wargor <webrooster@webrooster.ch>
2022-02-18 15:23:39 +0100

    Breadcrumbs -> changed clip to hidden, remove obsolte include

--- source/assets/svelte/src/components/shared/list/ListingNormal.svelte
+++ source/assets/svelte/src/components/shared/list/ListingNormal.svelte
@@ -3,7 +3,6 @@
 	import { _ } from 'svelte-i18n';
 	import SvelteMarkdown from 'svelte-markdown/src/SvelteMarkdown.svelte';
 	import Paragraph from '../Paragraph.svelte';
-	import Select from 'svelte-select/src/Select.svelte';
 
 	export let item: Item;
 	export let searchResultsHighlighting: any[];
