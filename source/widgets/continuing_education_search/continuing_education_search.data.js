a63ac2bdea5d4a33b952ef59a1d55f1fb1790567
Thomas Schallert <thomas.schallert@fhnw.ch>
2022-02-28 10:46:01 +0100

    cleanup data files and templates

--- source/widgets/continuing_education_search/continuing_education_search.data.js
+++ source/widgets/continuing_education_search/continuing_education_search.data.js
@@ -1,40 +1,35 @@
 'use strict';
 
 var _ = require('lodash'),
-	requireNew = require('require-new'),
-	dataHelper = require('../../../helpers/data.js'),
-	defaultData = requireNew('../../data/default.data.js');
+  requireNew = require('require-new'),
+  dataHelper = require('../../../helpers/data.js'),
+  defaultData = requireNew('../../data/default.data.js');
 
 var templateData = {
-		widget_data:
-			'{"filterSubjectArea": false, "filterDateLine": true, "filterLocation": true, "showImages": true, ' +
-			'"initialResultLayout": "teaser_layout", "subjectAreaOptions": [{"optionValue": "1003", "optionLabel": "International Studies", ' +
-			'"selected": false}, {"optionValue": "1009", "optionLabel": "Technik", "selected": false}], "dateLineOptions": [{"optionValue": "cas",' +
-			'"optionLabel": "CAS", "selected": false}, {"optionValue": "infoanlass", "optionLabel": "Info-Anlass", "selected": true}, ' +
-			'{"optionValue": "masterofarts", "optionLabel": "Master of Arts", "selected": false}], "locationOptions": [{"optionValue": "crrsav5xc4", ' +
-			'"optionLabel": "Andere", "selected": false}, {"optionValue": "basel", "optionLabel": "Basel", "selected": false}, ' +
-			'{"optionValue": "muttenz", "optionLabel": "Muttenz", "selected": false}], "subject": [], ' +
-			'"jsonURL": "http://localhost:8000/Plone/de/searchbar.json", "filterURL": "http://localhost:8000/Plone/de/searchbar.json"}',
-	},
-	data = _.merge(
-		defaultData,
-		{
-			meta: {
-				title: 'Continuing Education Search | WI_076',
-				description: '',
-				code: dataHelper.getTemplateCode('continuing_education_search.hbs'),
-				documentation: dataHelper.getDocumentation(
-					'continuing_education_search.md'
-				),
-				mocks: [
-					{
-						description: null,
-						data: dataHelper.getFormattedJSON(templateData),
-					},
-				],
-			},
-		},
-		templateData
-	);
+  title: 'Title add by PLONE',
+  description: 'Description added by PLONE',
+  widgetData: '',
+};
+
+var data = _.merge(
+  defaultData,
+  {
+    meta: {
+      title: 'Continuing Education Search | WI_076',
+      description: '',
+      code: dataHelper.getTemplateCode('continuing_education_search.hbs'),
+      documentation: dataHelper.getDocumentation(
+        'continuing_education_search.md'
+      ),
+      mocks: [
+        {
+          description: null,
+          data: dataHelper.getFormattedJSON(templateData),
+        },
+      ],
+    },
+  },
+  templateData
+);
 
 module.exports = data;
